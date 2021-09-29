8510932f08ab79683c0b873680c05744cccb4e06
Woojin Ahn <sky32678@gmail.com>
2021-09-29 14:15:31 -0500

    initial model parameters changed

--- control/anfis_codes/model.py
+++ control/anfis_codes/model.py
@@ -5,8 +5,210 @@ import torch.autograd
 import anfis_codes.anfis as anfis
 from anfis_codes.membership import make_tri_mfs, TriangularMembFunc, TrapezoidalMembFunc, make_bell_mfs, BellMembFunc, make_trap_mfs, Zero, make_zero
 from torch.autograd import Variable
+from anfis_codes.antecedent_layer import JointTrapMembershipV2, JointSingleConstrainedEdgeMembership, Joint7TrapMembership
 from anfis_codes.joint_mamdani_membership import JointSymmetricTriangleMembership,JointSymmetric9TriangleMembership
 import numpy as np
+
+def dist_target_dist_per_theta_lookahead_theta_far_theta_near():
+    """
+    0 dist target
+    1 dist line
+    2 theta lookahead
+    3 theta far
+    4 theta near
+    :return:
+    """
+
+    # FIXME for rules with uneven indices number i.e sometimes 2 or sometimes 3, pad the lesser ones with duplicates
+    variable_rule_index = [
+        (0, 2, 0),
+        (0, 2, 0),
+        (0, 2, 0),
+
+        (0, 1, 3),
+        (0, 1, 3),
+        (0, 1, 3),
+        (0, 1, 3),
+        (0, 1, 3),
+
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+
+        # Other side
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+        (0, 1, 4),
+
+        (0, 1, 3),
+        (0, 1, 3),
+        (0, 1, 3),
+        (0, 1, 3),
+        (0, 1, 3),
+
+        (0, 2, 0),
+        (0, 2, 0),
+        (0, 2, 0),
+    ]
+
+    '''
+    0 = near
+    1 = far
+
+    0 = far left
+    1 = close left
+    2 = zero
+    3 = close right
+    4 = far right
+
+    0 = far left
+    1 = near left
+    1 = close left
+    2 = zero
+    3 = close right
+    3 = near right
+    4 = far right
+    '''
+    membership_indices = [
+        (0, 0, 0),  # 1
+        (0, 1, 0),  # 2
+        (0, 2, 0),  # 3
+
+        (1, 0, 0),  # 3
+        (1, 0, 1),  # 3
+        (1, 0, 2),  # 3
+        (1, 0, 3),  # 3
+        (1, 0, 4),  # 3
+
+        (1, 1, 0),  # 3
+        (1, 1, 1),  # 3
+        (1, 1, 2),  # 3
+        (1, 1, 3),  # 3
+        (1, 1, 4),  # 3
+
+        (1, 2, 0),  # 3
+        (1, 2, 1),  # 3
+        (1, 2, 2),  # 3
+        (1, 2, 3),  # 3
+        (1, 2, 4),  # 3
+
+        (1, 3, 0),  # 3
+        (1, 3, 1),  # 3
+        (1, 3, 2),  # 3
+        (1, 3, 3),  # 3
+        (1, 3, 4),  # 3
+
+        # OTHER SIDE
+
+        (1, 4, 0),  # 3
+        (1, 4, 1),  # 3
+        (1, 4, 2),  # 3
+        (1, 4, 3),  # 3
+        (1, 4, 4),  # 3
+
+        (1, 5, 0),  # 3
+        (1, 5, 1),  # 3
+        (1, 5, 2),  # 3
+        (1, 5, 3),  # 3
+        (1, 5, 4),  # 3
+
+        (1, 6, 0),  # 3
+        (1, 6, 1),  # 3
+        (1, 6, 2),  # 3
+        (1, 6, 3),  # 3
+        (1, 6, 4),  # 3
+
+        (0, 2, 0),
+        (0, 3, 0),
+        (0, 4, 0),
+    ]
+
+    '''
+    0 = far left
+    1 = left
+    2 = close left
+    3 = zero
+    4 = close right
+    5 = right
+    6 = far right
+    '''
+    outputs_membership = [
+        (8,),  # 1
+        (6,),  # 1
+        (4,),  # 1
+        (8,),  # 1
+        (5,),  # 1
+        (4,),  # 1
+        (3,),  # 1
+        (0,),  # 1
+
+        (8,),  # 1
+        (7,),  # 2
+        (6,),  # 3
+        (5,),  # 4
+        (2,),  # 5
+        (8,),  # 6
+        (6,),  # 7
+        (5,),  # 8
+        (4,),  # 9
+        (1,),  # 10
+        (7,),  # 11
+        (5,),  # 12
+        (4,),  # 13
+        (3,),  # 14
+        (1,),  # 15
+
+        # OTHER SIDE
+
+        (7,),  # 1
+        (4,),  # 2
+        (3,),  # 3
+        (2,),  # 4
+        (0,),  # 5
+        (6,),  # 6
+        (3,),  # 7
+        (2,),  # 8
+        (1,),  # 9
+        (0,),  # 10
+        (8,),  # 11
+        (5,),  # 12
+        (4,),  # 13
+        (3,),  # 14
+        (0,),  # 15
+        (4,),  # 15
+        (2,),  # 15
+        (0,),  # 15
+    ]
+
+    mamdani_ruleset = {
+        'variable_rule_index': variable_rule_index,
+        'membership_indices': membership_indices,
+        'outputs_membership': outputs_membership
+    }
+
+    return mamdani_ruleset
 
 class Critic(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -31,6 +233,25 @@ class Anfis(nn.Module):
     def __init__(self):
         super(Anfis, self).__init__()
     def my_model(self):
+        parameter_values = [
+            [0, 1],
+
+            [0, 2, .1, .2, .2],
+            [0, 1, .6, 0.6],
+            [0, 0.976657846180786, 0.27020001411438, 0.1281498670578],
+            [0, 1.52320299803035, 0.081358410418034, 0.103709816932678],  #
+
+            [0, 1, 1, 1, 1]
+        ]
+
+        x_joint_definitons = [
+            ('distance_target', JointSingleConstrainedEdgeMembership(*parameter_values[0], constant_center=False)),
+
+            ('distance_line', Joint7TrapMembership(*parameter_values[1], constant_center=True)),
+            ('theta_lookahead', JointTrapMembershipV2(*parameter_values[2], constant_center=True)),
+            ('theta_far', JointTrapMembershipV2(*parameter_values[3], constant_center=True)),
+            ('theta_near', JointTrapMembershipV2(*parameter_values[4], constant_center=True)),
+        ]
         invardefs = [
                 ##three inputs
                 # ('distance_line', [TrapezoidalMembFunc(-100, -100, -1.947, -1.2515,1),
@@ -144,11 +365,17 @@ class Anfis(nn.Module):
                 #                 TrapezoidalMembFunc(0.99, 1.71, np.pi, np.pi,5),
                 #                 Zero()]),
 
-                ('theta_lookahead', [TrapezoidalMembFunc(-np.pi, -np.pi, -2.9, -1.9,1),
-                                TrapezoidalMembFunc( -2.9, -1.9, -1.7, -0.2,2),
-                                TrapezoidalMembFunc(-1.7, -0.2, 0.2, 1.7,3),
-                                TrapezoidalMembFunc(0.2, 1.7, 1.9, 2.9,4),
-                                TrapezoidalMembFunc(1.9, 2.9, np.pi, np.pi,5),
+                # ('theta_lookahead', [TrapezoidalMembFunc(-np.pi, -np.pi, -2.9, -1.9,1),
+                #                 TrapezoidalMembFunc( -2.9, -1.9, -1.7, -0.2,2),
+                #                 TrapezoidalMembFunc(-1.7, -0.2, 0.2, 1.7,3),
+                #                 TrapezoidalMembFunc(0.2, 1.7, 1.9, 2.9,4),
+                #                 TrapezoidalMembFunc(1.9, 2.9, np.pi, np.pi,5),
+                #                 Zero()]),
+                ('theta_lookahead', [TrapezoidalMembFunc(-np.pi, -np.pi, -2.4, -1.4,1),
+                                TrapezoidalMembFunc( -2.4, -1.4, -1.2, -0.2,2),
+                                TrapezoidalMembFunc(-1.2, -0.2, 0.2, 1.2,3),
+                                TrapezoidalMembFunc(0.2, 1.2, 1.4, 2.4,4),
+                                TrapezoidalMembFunc(1.4, 2.4, np.pi, np.pi,5),
                                 Zero()]),
 
                 ('distance', [TrapezoidalMembFunc(0, 0, 0.5, 0.7,1,2),
@@ -201,15 +428,15 @@ class Anfis(nn.Module):
 
         outvars = ['control_law']
 
-        mamdani_out = [
-            ('right3', [TriangularMembFunc(-1.25, -1, -0.75)]),
-            ('right2', [TriangularMembFunc(-1, -0.75, -0.5)]),
-            ('rigt1', [TriangularMembFunc(-0.75, -0.5, 0)]),
-            ('zero', [TriangularMembFunc(-0.5, 0, 0.5)]),
-            ('left1', [TriangularMembFunc(0, 0.5, 0.75)]),
-            ('left2', [TriangularMembFunc(0.5, 0.75, 1)]),
-            ('left3', [TriangularMembFunc(0.75, 1, 1.25)])
-                                                                ]
+        # mamdani_out = [
+        #     ('right3', [TriangularMembFunc(-1.25, -1, -0.75)]),
+        #     ('right2', [TriangularMembFunc(-1, -0.75, -0.5)]),
+        #     ('rigt1', [TriangularMembFunc(-0.75, -0.5, 0)]),
+        #     ('zero', [TriangularMembFunc(-0.5, 0, 0.5)]),
+        #     ('left1', [TriangularMembFunc(0, 0.5, 0.75)]),
+        #     ('left2', [TriangularMembFunc(0.5, 0.75, 1)]),
+        #     ('left3', [TriangularMembFunc(0.75, 1, 1.25)])
+        #                                                         ]
 
         input_keywords = [] #list
         number_of_mfs = {} #dict
@@ -220,5 +447,8 @@ class Anfis(nn.Module):
         # mamdani_out = JointSymmetricTriangleMembership(0, .9726, .007, .2036)
         # mamdani_out = JointSymmetric9TriangleMembership(0,-0.2287,1.0375,0.4749,0.4709)
         mamdani_out = JointSymmetric9TriangleMembership(0,1,1,1,1)
+        # Marius
+        # ruleset = dist_target_dist_per_theta_lookahead_theta_far_theta_near()
+        # anf = anfis.AnfisNetV2('ANFIS', x_joint_definitons , outvars, mamdani_out, input_keywords, number_of_mfs, False, ruleset)
         anf = anfis.AnfisNet('ANFIS', invardefs, outvars, mamdani_out, input_keywords, number_of_mfs, False)
         return anf
